24f3b0c468807f3dba819f1ad1fc54b5f12e8826
mukesh9467 <it.mukesh@gmail.com>
2026-03-03 23:13:43 +0530

    Updates

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,12 +5,10 @@ from app import app
 client = TestClient(app)
 
 
-
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert "message" in response.json()
-
 
 
 def test_create_item():
