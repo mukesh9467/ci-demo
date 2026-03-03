729687c503d68fdb1f57cb7717e996e30c85e460
mukesh9467 <it.mukesh@gmail.com>
2026-03-03 23:07:56 +0530

    Updates

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
-
 
 class Item(BaseModel):
     name: str
     price: float
 
-
 @app.get("/")
 def read_root():
     return {"message": "Python CI/CD Demo App Running"}
-
 
 @app.post("/items")
 def create_item(item: Item):
